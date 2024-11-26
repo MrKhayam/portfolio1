f97e4d4d61a69003436de51e68dd9e437b274ed9
Khayam Ijaz <ijazkhayam@gmail.com>
2024-11-26 22:53:49 +0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,16 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 
 const Hero = () => {
+  const info = {
+    name: "Khayam",
+    fullName: "Khayam Ijaz",
+  }
   return (
     <>
       <div className="md:w-full h-auto flex items-center flex-col">
-        <h1 className='font-["Charley"] md:text-9xl sm:text-8xl text-7xl text-white'>Khayam</h1>
+        <h1 className='font-["Charley"] md:text-9xl sm:text-8xl text-7xl text-white'>{info.name}</h1>
         <div className="w-full p-2 flex justify-center items-center">
-        <h1 className='md:text-3xl w-full text-[16px] sm:text-2xl md:flex justify-center font-bold text-white text-center'>Hello, I am Khayam Ijaz. A Fullstack <br/> Web Developer.</h1>
+        <h1 className='md:text-3xl w-full text-[16px] sm:text-2xl md:flex justify-center font-bold text-white text-center'>Hello, I am {info.fullName}. A Fullstack <br/> Web Developer.</h1>
         </div>
         <div className="btns flex w-full flex-col sm:flex-row md:flex-row items-center gap-2 md:gap-5 mt-5 justify-center">
             <button className='bg-[#202020] md:w-auto w-[95%] hover:bg-[#272727] transition-all duration-300 hover:text-white px-4 py-3 rounded-md text-[#dadada]'>Download Resume</button>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+
+
+
+
+
+
+const WhatsAppButton = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const phoneNumber = '03175080524';
+    const messageToSend = encodeURIComponent('');
+  
+    const handleClick = () => {
+      window.open(`https://wa.me/${phoneNumber}?text=${messageToSend}`, '_blank');
+    };
+  
+    return (
+        <button onClick={handleClick} className='bg-[#202020] md:w-auto w-[90%] justify-center hover:bg-[#272727] transition-all duration-300 hover:text-white px-4 py-3 flex items-center gap-2 rounded-md text-[#dadada]'>Send <FaArrowRightLong size={20} /></button>
+    );
+  };
+
+
+
+
+
+
+
 const Portfolio = () => {
   return (
     <>
@@ -9,7 +39,18 @@ const Portfolio = () => {
         </div>
         
 
-
+        <div className="w-full h-full flex items-center justify-center">
+            <div className="contactFrom flex flex-col gap-3 items-center w-full h-auto">
+                <div className="flex md:flex-row flex-col items-center gap-3 w-full md:w-1/2">
+                    <input value={name} onChange={() => setName(e.target)} placeholder="Name" className="bg-[#2727275e] md:w-1/2 rounded-md w-[90%] border-none outline-none p-3 text-white" type="text" />
+                    <input placeholder="Email" className="bg-[#2727275e] md:w-1/2 rounded-md w-[90%] border-none outline-none p-3 text-white" type="text" />
+                </div>
+                <div className="message w-full flex justify-center md:w-1/2">
+                <textarea placeholder="Message" className="md:w-full w-[90%] bg-[#2727275e] resize-none rounded-lg outline-none border-none p-3 h-32 text-white" name="" id=""></textarea>
+                </div>
+        <WhatsAppButton />
+            </div>
+        </div>
 
         
       </div>
